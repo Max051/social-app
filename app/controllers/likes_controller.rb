23b01c160c9de2157e0b58a49403dbabe59b770c
Kacper <ciecinski.kacper@gmail.com>
2016-11-12 20:05:25 +0100

    Unlike added, avatar fixed

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,8 +4,14 @@ class LikesController < ApplicationController
 if @like.save
 redirect_to :back
 else
+  if Like.where("user_id= ? AND micropost_id = ? OR comment_id = ?",current_user.id,@like.micropost_id,@like.comment_id)
+
+    Like.where("user_id= ? AND micropost_id = ? OR comment_id = ?",current_user.id,@like.micropost_id,@like.comment_id).destroy_all
+redirect_to :back
+  else
   flash[:danger] = "Something went wrong"
  redirect_to root_url
+ end
 end
   end
   private
